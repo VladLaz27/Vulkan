0d64182f8c10ce7cf3a575ee7acc2c67d1f016e5
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-12-11 14:58:33 +0200

    menu

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
-    $(document).ready(function(){
-  $('.show-menu').on('click', function(){
-    $('.vul-menu').toggleClass('expand');
-    return false;
-  });
+$(document).ready(function(){
+    $(".button a").click(function(){
+        $(".overlay").fadeToggle(200);
+       $(this).toggleClass('btn-open').toggleClass('btn-close');
+    });
+});
+$('.overlay').on('click', function(){
+    $(".overlay").fadeToggle(200);   
+    $(".button a").toggleClass('btn-open').toggleClass('btn-close');
+    open = false;
 });
