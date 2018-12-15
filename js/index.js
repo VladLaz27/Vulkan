1e7594953193bb2197a0b730e1241bb2654e609b
VladLaz27 <39669227+VladLaz27@users.noreply.github.com>
2018-12-15 10:11:41 +0200

    Проблємка

--- js/index.js
+++ js/index.js
@@ -9,3 +9,13 @@ $('.overlay').on('click', function(){
     $(".button a").toggleClass('btn-open').toggleClass('btn-close');
     open = false;
 });
+
+
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 6,
+      centeredSlides: true,
+      autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+      },
+    });
